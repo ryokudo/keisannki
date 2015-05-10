aae9890b9b89df605309d700a2a0272253dc0fdc
ryokudo <kr810.com@gmail.com>
2015-05-10 22:20:57 +0900

    ボタン作成（メソッド利用なし）

--- keisannki/keisannki/ViewController.swift
+++ keisannki/keisannki/ViewController.swift
@@ -13,6 +13,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let button = UIButton()
+        
+        //表示されるテキスト
+        button.setTitle("1", forState: .Normal)
+        
+        //テキストの色
+        button.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        
+        //タップした状態のテキスト
+        button.setTitle("1", forState: .Highlighted)
+        
+        //タップした状態の色
+        button.setTitleColor(UIColor.redColor(), forState: .Highlighted)
+        
+        //サイズ
+        button.frame = CGRectMake(0, 0, 50, 50)
+        
+        //配置場所
+        button.layer.position = CGPoint(x:100, y:100)
+        
+        //背景色
+        button.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+        
+        //角丸
+        button.layer.cornerRadius = 0
+        
+        //ボーダー幅
+        button.layer.borderWidth = 2
+        
+        //ボタンをタップした時に実行するメソッドを指定
+        button.addTarget(self, action: "tapped:", forControlEvents:.TouchUpInside)
+        
+        //viewにボタンを追加する
+        self.view.addSubview(button)
+        
+    }
+    
+    
+    
+    func tapped(button: UIButton) {
+            NSLog("test");
     }
 
     override func didReceiveMemoryWarning() {
