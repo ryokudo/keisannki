b11a3aec735dd4919827f2e2908a36d8f73bd420
ryokudo <kr810.com@gmail.com>
2015-05-10 21:50:33 +0900

    １回目

---
+++ keisannki/keisannki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  keisannki
+//
+//  Created by 工藤良 on 2015/05/10.
+//  Copyright (c) 2015年 工藤良. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
