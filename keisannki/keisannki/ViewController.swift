cbef49bade9b9dbe1adef64a840bc593525ff84a
ryokudo <kr810.com@gmail.com>
2015-05-10 22:33:20 +0900

    makeButtonメソッドを実装

--- keisannki/keisannki/ViewController.swift
+++ keisannki/keisannki/ViewController.swift
@@ -14,44 +14,54 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let button = UIButton()
+        let button1 = makeButton("1",x:50,y:50);
+        let button2 = makeButton("2",x:100,y:50);
+
         
-        //表示されるテキスト
-        button.setTitle("1", forState: .Normal)
-        
-        //テキストの色
-        button.setTitleColor(UIColor.blackColor(), forState: .Normal)
-        
-        //タップした状態のテキスト
-        button.setTitle("1", forState: .Highlighted)
-        
-        //タップした状態の色
-        button.setTitleColor(UIColor.redColor(), forState: .Highlighted)
-        
-        //サイズ
-        button.frame = CGRectMake(0, 0, 50, 50)
-        
-        //配置場所
-        button.layer.position = CGPoint(x:100, y:100)
-        
-        //背景色
-        button.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
-        
-        //角丸
-        button.layer.cornerRadius = 0
-        
-        //ボーダー幅
-        button.layer.borderWidth = 2
-        
-        //ボタンをタップした時に実行するメソッドを指定
-        button.addTarget(self, action: "tapped:", forControlEvents:.TouchUpInside)
+
         
         //viewにボタンを追加する
-        self.view.addSubview(button)
+        self.view.addSubview(button1)
+        self.view.addSubview(button2)
         
     }
     
-    
+    func makeButton(text:String, x:CGFloat, y:CGFloat) -> UIButton{
+            
+            let button = UIButton()
+            
+            //表示されるテキスト
+            button.setTitle(text, forState: .Normal)
+            
+            //テキストの色
+            button.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            //タップした状態のテキスト
+            button.setTitle(text, forState: .Highlighted)
+            
+            //タップした状態の色
+            button.setTitleColor(UIColor.redColor(), forState: .Highlighted)
+            
+            //サイズ
+            button.frame = CGRectMake(0, 0, 50, 50)
+            
+            //配置場所
+            button.layer.position = CGPoint(x:x, y:y)
+            
+            //背景色
+            button.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+            
+            //角丸
+            button.layer.cornerRadius = 0
+            
+            //ボーダー幅
+            button.layer.borderWidth = 2
+            
+            //ボタンをタップした時に実行するメソッドを指定
+            button.addTarget(self, action: "tapped:", forControlEvents:.TouchUpInside)
+            
+            return button
+    }
     
     func tapped(button: UIButton) {
             NSLog("test");
